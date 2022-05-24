8f97207f26fb076dbd2f4eae4646858a6be378ec
Ghedoni <elemigee@gmail.com>
2022-05-24 13:18:10 -0700

    Initial commit for printf

--- printf.c
+++ printf.c
@@ -6,23 +6,70 @@
  */
 int _printf(const char *format, ...)
 {
-	int i;
+	int len = 0;
+	f_id func_list[] = {
+		{"c", _print_char},
+		{"s", _putstr},
+		{"%", _print_percent},
+		{"d", _print_int},
+		{"i", _print_int},
+		{"b", _print_binary},
+		{NULL, NULL}
+	};
 	va_list arg;
 
-	va_start(arg, format);
-	if (format == NULL)
+	if (!format)
+		return (-1);
+	va_start(args, format);
+	len = _identifier(format, func_list, args);
+	va_end(args);
+	return (len);
+}
+
+/**
+ * _identifier - matches id to functions
+ * @format: Parameter
+ * @func_list: Parameter
+ * @args: Parameter
+ * Return: int
+ */
+int _identifier(const char *format, f_id func_list[], va_list args)
+{
+	int i, j, len = 0;
+
+	if (!format)
 		return (-1);
 	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[i] != '%')
-			_putchar(format[i]);
-		else
+		if (format[i] == '%')
 		{
-			if (format[i + 1] == 'c')
-				_putchar(va_arg(arg, int));
-			i++;
+			for (j = 0; func_list[j].id != NULL; j++)
+			{
+				if (format[i + 1] == func_list[j].id[0])
+				{
+					len += func_list[j].func(args);
+					break;
+				}
+			}
+			if (func_list[j].id == NULL && format[i + 1] != ' ')
+			{
+				if (format[i + 10] != '\0')
+				{
+					_putchar(format[i]);
+					_putchar(format[i + 1]);
+					len += 2;
+				}
+				else
+				{
+					return (-1);
+				}
+				i++;
+			}
+			else
+			{
+				_putchar(format[i]);
+				len++;
+			}
 		}
+		return (len);
 	}
-	return (0);
-	va_end(arg);
-}
