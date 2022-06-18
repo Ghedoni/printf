d71b978b08bf283db4067fc35dc07ebfa74f03fd
MaryBaptista <marybaptistanwachukwu@gmail.com>
2022-06-19 00:27:45 +0100

    updated correction

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 # printf
-*A joint repository.*
-
-This ia a project on printf for ALX 
--- Testf/main2.c
+++
@@ -1,7 +0,0 @@
-#include "../main.h"
-int main(void)
-{
-	printf("These are char: %c .. %c .. %c \n", 'o','n','e');
-	_printf("These are char: %c .. %c .. %c \n", 'o','n','e');
-	return (0);
-}
